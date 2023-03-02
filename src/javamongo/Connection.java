ea0e929f220d9b521dfd421cba85cfd3187d2613
LexzTemptation <alejandro_temptation@outlook.com>
2023-03-01 23:48:03 -0600

    Conexión exitosa...

--- src/javamongo/Connection.java
+++ src/javamongo/Connection.java
@@ -13,6 +13,8 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.Mongo;
+
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,13 +31,13 @@ public class Connection
         try
         {
             Mongo mongo = new Mongo("localhost", 27017);
-            BaseDatos = mongo.getDB("spring_test");
-            coleccion = BaseDatos.getCollection("persona");
+            BaseDatos = mongo.getDB("tienda");
+            coleccion = BaseDatos.getCollection("productos");
             System.out.println("Conexión exitosa...");
         }
-        catch (UnknownHostException exception)
+        catch (UnknownHostException e)
         {
-            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null.exception);
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null,e);
         }
     }
     
